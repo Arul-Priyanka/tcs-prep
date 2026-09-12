ec4b22da69ea88d49817238be904421834aa5ff2
Arul-Priyanka <priyankaganiaarul@gmail.com>
2026-09-12 19:19:15 +0530

    Rename Main class to RemoveVowels

---
+++ RemoveVowels.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class RemoveVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch != 'a' && ch != 'e' && ch != 'i' &&
+                ch != 'o' && ch != 'u') {
+                result += ch;
+            }
+        }
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
